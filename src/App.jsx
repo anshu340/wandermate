abe3b4ce540f246ce23cd96048b7c8a741b5fed9
anshu karki <anshukarki533@gmail.com>
2024-07-11 13:42:53 +0545

    big page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- import Landingpage 
-
+import LandingPage from "./components/LandingPage";
 function App() {
-  
-
   return (
     <>
-    <Router>
-      <Routes>
-          <Route path="/"element={<Landingpage/>}/>
-
-    
-      </Routes>
-    <Router/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
+export default App;
